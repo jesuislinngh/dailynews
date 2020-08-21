094414ff2ae85a9bb4812dbb40ec3734d58f167c
The doer <jesuislinn@gmail.com>
2020-08-21 16:28:04 -0500

    Adding the databinding code for the news feed fragment

--- app/src/main/java/com/example/android/mynewsapp/FragmentNews.kt
+++ app/src/main/java/com/example/android/mynewsapp/FragmentNews.kt
@@ -1,10 +1,13 @@
 package com.example.android.mynewsapp
 
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.mynewsapp.databinding.FragmentNewsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,28 +16,35 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentNews.newInstance] factory method to
- * create an instance of this fragment.
  */
 class FragmentNews : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var viewModel: DailyNewsViewModel
     //private var param1: String? = null
     // var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        /* arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        } */
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        viewModel = ViewModelProviders
+            .of(
+                requireActivity(),
+                DailyNewsViewModel.FACTORY(this.requireContext().applicationContext as Application)
+            )
+            .get(DailyNewsViewModel::class.java)
+
+        val binding = FragmentNewsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.newsFeed.adapter =
+            AdapterNewsFeed(AdapterNewsFeed.OnClickListener {
+                //viewModel.setDefinition(it.defid)
+
+                // TODO here we'll implement the logic to open the web browser.
+            })
+
+        return binding.root
     }
 
     /* companion object {
